86208ef256526e1311bbbfd1aeb910b56d0d5902
Rubin Bhandari <mohanbhandari@takeo.ai>
2022-08-27 12:07:20 +0545

    feat: init

--- README.md
+++ README.md
@@ -1,31 +1,40 @@
 # sample-env
 
+<p align="center">
+<a href="https://www.npmjs.com/package/sample-env"><img src="https://img.shields.io/npm/v/sample-env" alt="NPM Version" /></a>
+<a href="https://www.npmjs.com/package/sample-env"><img src="https://img.shields.io/npm/dm/sample-env" alt="NPM Downloads" /></a>
+  
+# About
+Easily generate sample environment files for your projects.
 Running sample-env will remove all secrets and only generate an env file with the following variables in your env
 
-```
-
-## Usage
+## How to install
 
 - Install
 
-```npm i sample-env --save-dev```
-
-- Add this to your scripts in package.json
-
-```
-"scripts": {
-  "sample-env": "sample-env"
-},
+```sh
+npm i sample-env -g
 ```
 
-- Run as
+## Usage
+  
+  ```sh
+    sample-env
+  ```
 
-```npm run sample-env```
+## Supported arguments and commands
+The cli takes these optional flags.
+* --env (default .env) - use this option if you want to use a different file name
+  * `--env .env.dev`
+* --sample (default .env.sample) - use this option if you want to use a output file name
+  * `--sample .env.example`
+* --banner - use this option if you want to add a custom banner text at start of file
+  * `--banner "hello world"`
+* --removeComments - use this option if you don't want to include comments in the file
+  * `--removeComments`
 
-## API
-The command takes 2 optional flags.
-* --env (default .env) - if your .env file is called something else, you can use this flag to change the name like 
-  * `--env .enviroment`
-
-* --sample (default .env-sample) - the filename to generate, like 
-  * `--sample .env-baseline`
+## Help Usage
+  
+  ```sh
+    sample-env --help
+  ```
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,68 @@
 #! /usr/bin/env node
 
-import fs from "fs";
-import readline from "readline";
-import yargs from 'yargs/yargs';
-import { hideBin } from 'yargs/helpers';
-import {splitLine} from "./utils";
+import fs from 'fs'
+import readline from 'readline'
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+import pkg from '../package.json'
+import { splitLine } from './utils'
 
-
-interface IOptions{
-  env?: string;
-  sample?: string;
+interface Options {
+  env?: string
+  sample?: string
+  banner?: string
+  removeComments?: boolean
 }
 
-const args = <IOptions>yargs(hideBin(process.argv)).argv
+const args = <Options>yargs(hideBin(process.argv))
+  .usage('Usage: $0 [options]')
+  .help('help').alias('help', 'h')
+  .version('version', pkg.version).alias('version', 'v')
+  .options({
+    env: {
+      description: '<filename> input file name',
+      requiresArg: true,
+      required: false,
+    },
+    sample: {
+      description: '<filename> output file name',
+      requiresArg: true,
+      required: false,
+    },
+    banner: {
+      description: 'add banner to output file',
+      requiresArg: true,
+      required: false,
+    },
+    removeComments: {
+      description: 'removes comment from output file',
+      requiresArg: false,
+      required: false,
+      boolean: true,
+    },
+  })
+  .argv
 
-const envPath = args.env || ".env"
-const baselinePath = args.sample || ".env-sample"
+const envPath = args.env || '.env'
+const samplePath = args.sample || '.env.sample'
+const fileStream = fs.createWriteStream(samplePath)
 
+const reader = readline.createInterface({
+  input: fs.createReadStream(envPath),
+  crlfDelay: Infinity,
+})
 
-const fileStream = fs.createWriteStream(baselinePath);
+if (args.banner)
+  fileStream.write(`${args.banner}\n`)
 
-const rl = readline.createInterface({
-	input: fs.createReadStream(envPath),
-	crlfDelay: Infinity,
-});
+reader.on('line', (line) => {
+  if (line.length === 0)
+    fileStream.write('\n')
+  else if (line.startsWith('#') && args.removeComments)
+    fileStream.write('\n')
 
-rl.on("line", (line) => {
-	if (line.length === 0) {
-		fileStream.write("\n");
-	} else {
-		fileStream.write(splitLine(line) + "\n");
-	}
-});
+  else
+    fileStream.write(`${splitLine(line)}\n`)
+})
+
+console.info('\x1B[32m%s\x1B[0m', `✅ Successfully generated file ${samplePath}`)
